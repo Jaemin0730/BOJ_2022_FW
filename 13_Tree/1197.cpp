b1892f5ed199a436aae083ca127fa80cd4953a7f
Jaemin0730 <jaemkim01@hanyang.ac.kr>
2022-07-28 00:08:16 +0900

    2022.07.27 푼 트리 문제

---
+++ 13_Tree/1197.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+#define MAX 10001
+vector<pii> node[MAX];
+priority_queue<pii> pq;
+bool tf[MAX] = {};
+
+int main(void)
+{
+    u;
+    int v, e;
+    cin >> v >> e;
+}
