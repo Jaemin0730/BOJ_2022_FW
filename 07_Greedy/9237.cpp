9bf9d896f20250aa5b008d80790de3e8a4b1e133
Jaemin0730 <jaemkim01@hanyang.ac.kr>
2022-08-13 11:59:23 +0900

    2022.08.13 푼 그리디 문제

---
+++ 07_Greedy/9237.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define u ios::sync_with_stdio(false); cin.tie(NULL)
+#define fst first
+#define snd second
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main(void)
+{
+    u;
+    int n;
+    int a[100000];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+}
